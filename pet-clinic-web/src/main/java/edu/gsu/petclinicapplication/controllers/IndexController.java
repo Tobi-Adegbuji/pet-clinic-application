780e7e310433030e1a2eaf19d92029338b7b3087
Tobi Adegbuji <Adegbuji_tobi@yahoo.com>
2020-05-05 15:22:50 -0400

    Added index page and its controller. Closes #8

---
+++ pet-clinic-web/src/main/java/edu/gsu/petclinicapplication/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package edu.gsu.petclinicapplication.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    //When a request comes into the root context,/,index, or index.html, they will match to this request mapping
+    @RequestMapping({"","/","index", "index.html"})
+    public String index(){
+        return "index";
+    }
+
+}
