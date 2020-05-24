a6d6b6f290681f6b7d138e61d8a615b5f8774f82
Tobi Adegbuji <Adegbuji_tobi@yahoo.com>
2020-05-24 08:22:41 -0400

    Added Owners List. Updated Owner Controller. Closes #16

--- pet-clinic-web/src/main/java/edu/gsu/petclinicapplication/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/edu/gsu/petclinicapplication/controllers/OwnerController.java
@@ -1,13 +1,24 @@
 package edu.gsu.petclinicapplication.controllers;
 
+import edu.gsu.petclinicapplication.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-@RequestMapping("/owners")  //<---shortcut if you dont want to keep typing owners in the below request. (Basically prefixing)
+@RequestMapping("/owners")  //<---shortcut if you dont want to keep typing owners in the below requestMapping. (Basically prefixing)
 @Controller
 public class OwnerController {
 
+    //Declared final so the object can never be changed
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"}) //<-- What client will request or type
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index"; //<---The name of view that we return to front controller to give to the view
     }
 
