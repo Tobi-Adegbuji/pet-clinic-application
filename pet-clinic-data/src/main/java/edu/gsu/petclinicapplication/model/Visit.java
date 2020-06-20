a7cab8e678fcb480044be5ab7a96ed3d841b51af
Tobi Adegbuji <Adegbuji_tobi@yahoo.com>
2020-06-20 13:01:32 -0400

    updated visits with JPA entity mappings. Closes #38

--- pet-clinic-data/src/main/java/edu/gsu/petclinicapplication/model/Visit.java
+++ pet-clinic-data/src/main/java/edu/gsu/petclinicapplication/model/Visit.java
@@ -1,11 +1,19 @@
 package edu.gsu.petclinicapplication.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit {
-
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
@@ -31,4 +39,7 @@ public class Visit {
     public void setPet(Pet pet) {
         this.pet = pet;
     }
+
+
+
 }
