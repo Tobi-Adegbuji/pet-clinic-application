41da3347fcf8156605e93292947b6ead59767682
Tobi Adegbuji <Adegbuji_tobi@yahoo.com>
2020-06-20 10:44:26 -0400

    Updated base entity with JPA annotations. Closes #35

--- pet-clinic-data/src/main/java/edu/gsu/petclinicapplication/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/edu/gsu/petclinicapplication/model/BaseEntity.java
@@ -1,5 +1,8 @@
 package edu.gsu.petclinicapplication.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
@@ -11,6 +14,9 @@ So all of our objects have this id property. This closes issue 10 on Github
 
 @MappedSuperclass  //Tells JPA that other classes will inherit this class
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Identity indicates that the persistence provider must assign pk's for the entity using a database identity column
     private Long id; //Hibernate recommends using box types since they can be null
 
     public Long getId() {
